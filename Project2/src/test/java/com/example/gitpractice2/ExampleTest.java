1d250aade8d5ad8603607b1dd14527c0236cf73e
Evgeny Per <kunnka123.one123@gmail.com>
2024-12-04 14:46:36 +0300

    Added new feature

---
+++ Project2/src/test/java/com/example/gitpractice2/ExampleTest.java
@@ -0,0 +1,12 @@
+package com.example.gitpractice2;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void testExample() {
+        assertEquals(2 + 2, 4);
+    }
+}
